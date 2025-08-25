68e29582236731d35615dbd18cb7210ae7c82aff
Taesin Moon <kjk06119@gmail.com>
2025-08-25 17:45:21 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.09 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.java
@@ -1,16 +1,11 @@
 class Solution {
     public int solution(int n, String control) {
-        for(int i = 0 ; i < control.length() ; i++){
-            char c = control.charAt(i);
+        for(char c : control.toCharArray()){
             switch(c){
-                case 'w': n++;
-                    break;
-                case 's': n--;
-                    break;
-                case 'd': n += 10;
-                    break;
-                case 'a': n -= 10;
-                    break;
+                case 'w': n++; break;
+                case 's': n--; break;
+                case 'd': n += 10; break;
+                case 'a': n -= 10; break;
             }
         }
         return n;
