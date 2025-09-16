bb407546ae7a5f0194e62909e26e3b60607f3b4b
Taesin Moon <kjk06119@gmail.com>
2025-09-16 18:27:00 +0900

    [level 0] Title: n 번째 원소부터, Time: 3.44 ms, Memory: 90.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = n-1 ; i < num_list.length ; i++) list.add(num_list[i]);
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
