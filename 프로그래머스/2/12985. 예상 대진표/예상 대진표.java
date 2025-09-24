af78fef7201205d8a5f321ae2855a8dac691f82c
Taesin Moon <kjk06119@gmail.com>
2025-09-24 17:18:36 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,23 +1,15 @@
-import java.util.*;
-
 class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int round = 1;
-        while(true){
-            int max = Math.max(a,b);
-            int min = Math.min(a,b);
-            if(min%2 == 1 && max == min+1) break;
-            
-            if(a%2 == 1) a = a/2 + 1;
-            else a /= 2;
-            
-            if(b%2 == 1) b = b/2 + 1;
-            else b /= 2;
-            
-            round++;
+        int answer = 1;
+        
+        while(!((a%2==1 && b==a+1) || (b%2==1 && a==b+1))) {
+            a = (a+1) / 2;
+            b = (b+1) / 2;
+            answer++;
         }
-        return round;
+
+        return answer;
     }
 }
