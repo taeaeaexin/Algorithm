6085ae4676efaa2d058121aa94ea25dd9e5f0409
Taesin Moon <kjk06119@gmail.com>
2025-09-26 18:58:13 +0900

    [level 2] Title: 전화번호 목록, Time: 284.56 ms, Memory: 227 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        // sort
+        HashMap<String, Integer> map = new HashMap<>();
+        for(int i = 0 ; i < phone_book.length ; i++){
+            map.put(phone_book[i], i);
+        }
+        
+        for(String str : phone_book){
+            for(int i = 0 ; i < str.length() ; i++){
+                if(map.containsKey(str.substring(0,i))) return false;
+            }
+        }
+
+        return true;
+    }
+}
