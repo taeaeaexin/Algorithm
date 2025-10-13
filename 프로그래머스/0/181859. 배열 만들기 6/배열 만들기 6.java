4f7205603f1f24926351e6bc2ca527dcf28e6363
Taesin Moon <kjk06119@gmail.com>
2025-10-13 18:02:19 +0900

    [level 0] Title: 배열 만들기 6, Time: 38.67 ms, Memory: 121 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        ArrayList<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length){
+            if(stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+            }else if(stk.get(stk.size()-1) == arr[i]){
+                stk.remove(stk.size()-1);
+                i++;
+            }else{
+                stk.add(arr[i]);
+                i++;
+            }
+        }
+        
+        if(stk.isEmpty()) return new int[]{-1};
+        return stk.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
