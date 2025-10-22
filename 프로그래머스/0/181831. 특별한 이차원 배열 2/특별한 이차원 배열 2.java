00e6c3a2a2b92ddd59372594eb1eb35ceb3c7e80
Taesin Moon <kjk06119@gmail.com>
2025-10-22 11:11:05 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.44 ms, Memory: 100 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[][] arr) {
+        boolean flag = true;
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr[0].length ; j++){
+                if(arr[i][j] != arr[j][i]) flag = false;
+            }
+        }
+        return flag?1:0;
+    }
+}
