dec6341de07fe01e42cd6f2d3d752698652cedca
Taesin Moon <kjk06119@gmail.com>
2025-10-26 15:58:10 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.02 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        for(int n : num_list){
+            if(n%2==0) answer[0]++;
+            else answer[1]++;
+        }
+        return answer;
+    }
+}
